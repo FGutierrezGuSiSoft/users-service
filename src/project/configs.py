e2674f00687b6d1ac6e6e12579de78d8af3b329f
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2019-07-05 21:00:58 -0400

    DB config on envs

--- src/project/configs.py
+++ src/project/configs.py
@@ -1,2 +1,8 @@
+import os
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgres://diego:Diego123@electivoespecialidadi.cm0fytbjzmgm.us-east-1.rds.amazonaws.com:5432/diego'
+    SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@electivoespecialidadi.cm0fytbjzmgm.us-east-1.rds.amazonaws.com:5432/{}'.format(
+        os.getenv('DB_USER'),
+        os.getenv('DB_PASSWORD'),
+        os.getenv('DB_NAME'))
